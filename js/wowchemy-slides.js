c120b9f670e13c79607f41c77259255b74cc2bb6
davidaparicio <davidaparicio@users.noreply.github.com>
2021-09-17 15:29:10 +0000

    deploy: 5b1b7be8526cc8289c70698ca6cb8675d713aacf

--- js/wowchemy-slides.js
+++ js/wowchemy-slides.js
@@ -1,6 +1,6 @@
 (() => {
   // ns-params:@params
-  var slides = { highlight_style: "dracula", theme: "black" };
+  var slides = { highlight_style: "dracula", theme: "white" };
 
   // ns-hugo:/tmp/hugo_cache/modules/filecache/modules/pkg/mod/github.com/wowchemy/wowchemy-hugo-modules/wowchemy/v5@v5.0.0-20210830150813-8b6612e7631c/assets/js/wowchemy-utils.js
   function fixMermaid(render = false) {
