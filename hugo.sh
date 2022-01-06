6c9eac13c63cabc845ac5db3378db34050726e44
David Aparicio <david.aparicio@free.fr>
2022-01-06 19:36:01 +0100

    👷 ci: clevercloud nano flavor

--- hugo.sh
+++ hugo.sh
@@ -9,15 +9,17 @@ tar xvf hugo_extended_"$HUGO_VERSION"_Linux-64bit.tar.gz
 chmod +x ./hugo
 ./hugo --gc --minify --destination "${HUGO_DESTINATION}" "${HUGO_DEBUG}"
 
-clever create --type static-apache website
+#clever create --type static-apache website
+clever scale --flavor nano
 #clever domain add clever-cloud.com/fr/podcast
 clever config set force-https enabled
 clever env set CC_PRE_BUILD_HOOK "./hugo.sh"
-clever env set CC_PRE_BUILD_HOOK "${BASH_SOURCE[0]}"
+#clever env set CC_PRE_BUILD_HOOK "${BASH_SOURCE[0]}"
 #clever env set CC_WEBROOT "/${HUGO_DESTINATION}"
-clever env set CC_WEBROOT "/"
+clever env set CC_WEBROOT "/public"
 clever env set HUGO_ENV "production"
 clever env set HUGO_VERSION "${HUGO_VERSION}"
+clever env set PORT "1313"
 clever link website
 clever deploy
 
