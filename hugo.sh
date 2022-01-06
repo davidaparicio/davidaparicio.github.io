b053a246aca55e1e4d4ccdde05e13125a6fe7610
David Aparicio <david.aparicio@free.fr>
2022-01-06 18:47:32 +0100

    🐛 fix: [ERROR] There are no applications linked. You can add one with `clever link`

--- hugo.sh
+++ hugo.sh
@@ -7,16 +7,18 @@ HUGO_DEBUG="" #--debug
 wget https://github.com/gohugoio/hugo/releases/download/v$HUGO_VERSION/hugo_extended_"$HUGO_VERSION"_Linux-64bit.tar.gz
 tar xvf hugo_extended_"$HUGO_VERSION"_Linux-64bit.tar.gz
 chmod +x ./hugo
-./hugo --gc --minify --destination "${HUGO_DESTINATION}"
+./hugo --gc --minify --destination "${HUGO_DESTINATION}" "${HUGO_DEBUG}"
 
-clever create --type static-apache mysite
+clever create --type static-apache website
 #clever domain add clever-cloud.com/fr/podcast
 clever config set force-https enabled
-#clever env set CC_PRE_BUILD_HOOK "./hugo.sh"
+clever env set CC_PRE_BUILD_HOOK "./hugo.sh"
 clever env set CC_PRE_BUILD_HOOK "${BASH_SOURCE[0]}"
-clever env set CC_WEBROOT "/${HUGO_DESTINATION}"
+#clever env set CC_WEBROOT "/${HUGO_DESTINATION}"
+clever env set CC_WEBROOT "/"
 clever env set HUGO_ENV "production"
 clever env set HUGO_VERSION "${HUGO_VERSION}"
+clever link website
 clever deploy
 
 # https://www.clever-cloud.com/blog/engineering/2020/06/18/deploy-static-site-hugo/
