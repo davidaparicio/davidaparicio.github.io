71bd3bde9b2b1dc10755bdf941379fae4dd1aca2
David Aparicio <david.aparicio@free.fr>
2022-01-07 10:25:14 +0100

    👷 ci: clevercloud

---
+++ deploy_clever.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+HUGO_VERSION="0.91.2"
+HUGO_DESTINATION="public"
+#HUGO_DEBUG="" #--debug
+#docker run --rm -it --cpus="1.5" -v "$(pwd)":/src klakegg/hugo:"${HUGO_VERSION}"-ext-alpine --cleanDestinationDir --minify ${HUGO_DEBUG}
+
+clever create "website_$(date "+%Y%m%dT%H%M%S")" --type static-apache
+clever scale --flavor nano
+#clever link website|app_24b9854c-98c7-4cdd-abec-2b0fb914cf3b
+#clever domain add clever-cloud.com/fr/podcast
+clever config set force-https enabled
+clever env set CC_PRE_BUILD_HOOK "./hugo.sh"
+#clever env set CC_PRE_BUILD_HOOK "${BASH_SOURCE[0]}"
+clever env set CC_WEBROOT "/${HUGO_DESTINATION}"
+clever env set HUGO_ENV "production"
+clever env set HUGO_VERSION "${HUGO_VERSION}"
+clever env set PORT "1313"
+BASE_URL="https://$(clever domain | xargs)"
+clever env set BASE_URL "${BASE_URL}"
+clever deploy
+
+# https://www.clever-cloud.com/blog/engineering/2020/06/18/deploy-static-site-hugo/
+# https://www.clever-cloud.com/doc/deploy/application/golang/go/
+# https://hub.docker.com/r/klakegg/hugo/
+# https://www.jhipster.tech/clever-cloud/
--- hugo.sh
+++ hugo.sh
@@ -1,28 +1,11 @@
 #!/usr/bin/env bash
 
-HUGO_VERSION="0.91.2"
-HUGO_DESTINATION="public"
-HUGO_DEBUG="" #--debug
+#HUGO_VERSION="0.91.2"
+#HUGO_DESTINATION="public"
+#HUGO_DEBUG="" #--debug
+#BASE_URL="$(clever domain)"
 
 wget https://github.com/gohugoio/hugo/releases/download/v$HUGO_VERSION/hugo_extended_"$HUGO_VERSION"_Linux-64bit.tar.gz
 tar xvf hugo_extended_"$HUGO_VERSION"_Linux-64bit.tar.gz
 chmod +x ./hugo
-./hugo --gc --minify --destination "${HUGO_DESTINATION}" "${HUGO_DEBUG}"
-
-#clever create --type static-apache website
-clever scale --flavor nano
-#clever domain add clever-cloud.com/fr/podcast
-clever config set force-https enabled
-clever env set CC_PRE_BUILD_HOOK "./hugo.sh"
-#clever env set CC_PRE_BUILD_HOOK "${BASH_SOURCE[0]}"
-#clever env set CC_WEBROOT "/${HUGO_DESTINATION}"
-clever env set CC_WEBROOT "/public"
-clever env set HUGO_ENV "production"
-clever env set HUGO_VERSION "${HUGO_VERSION}"
-clever env set PORT "1313"
-clever link website
-clever deploy
-
-# https://www.clever-cloud.com/blog/engineering/2020/06/18/deploy-static-site-hugo/
-# https://www.clever-cloud.com/doc/deploy/application/golang/go/
-# https://hub.docker.com/r/klakegg/hugo/
+./hugo --gc --minify --baseURL "${BASE_URL}" --destination "${HUGO_DESTINATION}" "${HUGO_DEBUG}"
