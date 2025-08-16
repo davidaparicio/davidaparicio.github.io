9f3fb2cdda3b2c75cd8af4790cfab6d02bc19220
davidaparicio <davidaparicio@users.noreply.github.com>
2025-08-16 17:48:22 +0000

    deploy: 62d7031f8ba336fe0c8091ef9818a2b014860636

---
+++ wheel/README.md
@@ -0,0 +1,67 @@
+# Wheel of Misfortune
+**Wheel of Misfortune** is a game that aims to build confidence in on-call engineers via simulated outage scenarios.
+With the game, you practice problem debugging under stress, understanding the incident management protocol,
+and effective communication with other engineers of your team and organization.
+It is a great way to train new hires, interns, and seasoned engineers to become well-rounded on-call engineers.
+
+The game is inspired by the [Site Reliability Engineering](https://landing.google.com/sre/book/chapters/accelerating-sre-on-call.html#xref_training_disaster-rpg) book.
+
+[Demo website](https://dastergon.gr/wheel-of-misfortune)
+
+## Instructions
+### Terminology
+
+*   **Scenario**: A past or fictional incident case.
+*   **Game Master**: The host-coordinator of the session.
+*   **Volunteer**: The trainee on-call engineer.
+
+Feel free to fork the [repository](https://github.com/dastergon/wheel-of-misfortune) or [download](https://github.com/dastergon/wheel-of-misfortune/releases) the stable release.
+Copy the [general\_incidents.json.sample](incidents/general\_incidents.json.sample) file to *general_incidents.json*, inside the [incidents/](incidents/) directory, and insert your incident scenarios into it.
+
+To run the game locally on your computer please navigate the the main directory of the downloaded project i.e. `wheel-of-misfortune-5.0/` and from here start a web server i.e. `python -m SimpleHTTPServer` after that open the http://localhost:8000 using your web browser.
+
+The file has the following format:
+- **ID**: the unique ID of the outage (you can just auto-increment).
+- **title**: the title of the incident.
+- **scenario**: the description of the incident. It is useful to include URLs from monitoring systems, dashboards, time-series databases and playbooks.
+- **inkstory**: the path to an [Ink](https://www.inklestudios.com/ink/) story file in JSON format.
+
+You can also use [general\_incidents.jsonnet](incidents/general_incidents.jsonnet.sample) as an example, in case you want to generate your incident scenarios using [Jsonnet](https://jsonnet.org/).
+
+### Ink
+[Ink](https://github.com/inkle/ink) is a scripting language for writing interactive narrative stories. It enables us to write interactive incident response narratives for team or individual trainings. You can use [Inky](https://github.com/inkle/inky) to write an interactive narrative for an incident and then export the story as JSON. Then, you can store the story file inside the [incidents/](incidents/) folder and associate the Ink story file with an Incident scenario using the **inkstory** key. You can read an example incident narrative [here](https://github.com/dastergon/wheel-of-misfortune/tree/master/incidents/redis-story.json).
+
+### Role Playing
+#### Game Master
+
+1.  Choose a volunteer to be the primary on-call engineer in front of the group.
+2.  Find a balance between the volunteer's experience and the incident's difficulty.
+3.  Assist volunteer by answering questions that may arise in each theoretical action or dashboard observation.
+  * Engage with the rest of the team and ask for different ways to debug the problem following the volunteer's explanation.
+  * Team members may be made available over time for assistance in various topics.
+5.  At the end, have a debrief on the learnings of the session.
+
+
+#### Volunteer
+
+1.  Spin the wheel and attempt to fix the theoretical outage scenario.
+2.  Explain to the Game Master and the rest of the group what actions you would take (lookup queries, checks in dashboards, etc.) to find the root causes, and eventually solve the incident.
+3.  Always keep an eye on the time, since it is a simulated incident response scenario and not a routine troubleshooting process. During a real incident, you might have an SLA or SLO breach and therefore you should take timing into account.
+4.  Engage with the rest of the group. Keep them in the loop. Ask questions to different members depending on their expertise.
+
+Most importantly, **have fun!**
+
+You can [read](https://landing.google.com/sre/book/chapters/accelerating-sre-on-call.html#xref_training_disaster-rpg) a comprehensive example on how to conduct the exercise in the Google SRE book.
+
+### Featured
+
+The Wheel of Misfortune was established as a practice in [Open Practice Library](https://openpracticelibrary.com/) and this project was [featured](https://openpracticelibrary.com/practice/wheel-of-misfortune/) there.
+
+### Resources
+
+*   [Disaster Role Playing](https://landing.google.com/sre/book/chapters/accelerating-sre-on-call.html#xref_training_disaster-rpg)
+*   [Managing Misfortune for Best Results](https://www.usenix.org/conference/srecon18europe/presentation/barry)
+*   [Postmortem Culture: Learning from Failure](https://landing.google.com/sre/book/chapters/postmortem-culture.html)
+*   [Postmortem Templates](https://github.com/dastergon/postmortem-templates)
+*   [Postmortems Metadata Index](https://postmortems.app)
+*   [Site Reliability Engineering Resources](https://github.com/dastergon/awesome-sre)
---
+++ wheel/static/volunteer.js
@@ -0,0 +1,146 @@
+/* This is a modified version of Jeremy Rue's Wheel of Fortune
+ * http://bl.ocks.org/jrue/a2aaf36b3c096925ccbf */
+
+ function custom_colors(n) {
+    var colors = ["#388E3C", "#1976D2", "#D32F2F", "#FFA000", "#388E3C", "#990099", "#0099c6", "#C2185B", "#81C784", "#D32F2F", "#1976D2", "#994499", "#4DD0E1", "#AED581", "#536DFE", "#FBC02D", "#C62828", "#AB47BC", "#66BB6A", "#90A4AE", "#0D47A1"];
+    return colors[n % colors.length];
+}
+
+var padding = { top: 20, right: 40, bottom: 0, left: 0 },
+    w = 500 - padding.left - padding.right,
+    h = 500 - padding.top - padding.bottom,
+    r = Math.min(w, h) / 2,
+    rotation = 0,
+    oldrotation = 0,
+    picked = 100000,
+    oldpick = [];
+
+    document.getElementById("participants").addEventListener("change", function () {
+        var input = this.value;
+        var data = input.split(",").map(function (item) {
+            return { title: item.trim() };
+        });
+    
+        // Clear the old wheel
+        d3.select("#wheel").selectAll("*").remove();
+    
+        var svg = d3.select("#wheel")
+            .append("svg")
+            .data([data])
+            .attr("viewBox", "0 0 500 500");
+    
+        var container = svg.append("g")
+            .attr("transform", "translate(" + (w / 2 + padding.left) + "," + (h / 2 + padding.top) + ")");
+    
+        var vis = container
+            .append("g");
+    
+        var pie = d3.layout.pie().sort(null).value(function (d) { return 1; });
+    
+        // declare an arc generator function
+        var arc = d3.svg.arc().outerRadius(r);
+    
+        // select paths, use arc generator to draw
+        var arcs = vis.selectAll("g.slice")
+            .data(pie)
+            .enter()
+            .append("g")
+            .attr("class", "slice");
+    
+        arcs.append("path")
+            .attr("fill", function (d, i) { return custom_colors(i); })
+            .attr("d", function (d) { return arc(d); });
+    
+        // add the text
+        arcs.append("text").attr("transform", function (d) {
+            d.innerRadius = 0;
+            d.outerRadius = r;
+            d.angle = (d.startAngle + d.endAngle) / 2;
+            return "rotate(" + (d.angle * 180 / Math.PI - 90) + ")translate(" + (d.outerRadius - 10) + ")";
+        })
+            .attr("text-anchor", "end")
+            .style("font-weight", "bold")
+            .text(function (d, i) {
+                return data[i].title;
+            });
+        container.on("click", spin);
+    
+        function spin(d) {
+            container.on("click", null);
+            //all slices have been seen, all done
+            if (oldpick.length == data.length) {
+                container.on("click", null);
+                return;
+            }
+        
+            var ps = 360 / data.length,
+                pieslice = Math.round(1440 / data.length),
+                rng = Math.floor((Math.random() * 1440) + 360);
+        
+            rotation = (Math.round(rng / ps) * ps);
+            picked = Math.round(data.length - (rotation % 360) / ps);
+            picked = picked >= data.length ? (picked % data.length) : picked;
+        
+            if (oldpick.indexOf(picked) !== -1) {
+                d3.select(this).call(spin);
+                return;
+            } else {
+                oldpick.push(picked);
+            }
+        
+            rotation += 90 - Math.round(ps / 2);
+            vis.transition()
+                .duration(3000)
+                .attrTween("transform", rotTween)
+                .each("end", function () {
+                    //mark incident as seen
+                    d3.select(".slice:nth-child(" + (picked + 1) + ") path")
+                        .attr("fill", "#111");
+                    //populate incident
+                    d3.select("#incident p")
+                        .html(
+                            "<h1 class=\"f1 center mw6\">" + data[picked].title + "</h1>"
+                        );
+        
+                    // Trigger confetti
+                    confetti({
+                        particleCount: 100,
+                        spread: 70,
+                        origin: { y: 0.6 }
+                    });
+        
+                    container.on("click", spin);
+        
+                    oldrotation = rotation;
+                });
+        }
+    
+        //make arrow
+        svg.append("g")
+            .attr("transform", "translate(" + (w + padding.left + padding.right) + "," + ((h / 2) + padding.top) + ")")
+            .append("path")
+            .attr("d", "M-" + (r * .15) + ",0L0," + (r * .05) + "L0,-" + (r * .05) + "Z")
+            .style({ "fill": "black" });
+    
+        //draw spin circle
+        container.append("circle")
+            .attr("cx", 0)
+            .attr("cy", 0)
+            .attr("r", 30)
+            .style({ "fill": "white", "cursor": "pointer" });
+    
+        //spin text
+        container.append("text")
+            .attr("x", 0)
+            .attr("y", 10)
+            .attr("text-anchor", "middle")
+            .text("SPIN")
+            .style({ "color": "white", "font-weight": "bold", "font-size": "18px" });
+    
+        function rotTween(to) {
+            var i = d3.interpolate(oldrotation % 360, rotation);
+            return function (t) {
+                return "rotate(" + i(t) + ")";
+            };
+        }
+    });
