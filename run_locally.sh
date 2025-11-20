135892cebe96662f315be72ef05d8b80df5a23de
David Aparicio <david.aparicio@free.fr>
2025-11-20 22:16:06 +0100

    👷 ci: enforce v0.121.2 hugo version

--- run_locally.sh
+++ run_locally.sh
@@ -8,4 +8,5 @@ BASE_URL=""
 #hugo --gc --minify --baseURL "${BASE_URL}" --destination "${HUGO_DESTINATION}" "${HUGO_DEBUG}"
 echo ""
 echo "--- RUNNING  ---"
-hugo server --port 1313 "${HUGO_DEBUG}" --baseURL "${BASE_URL}" #--i18n-warnings --disableFastRender 
+/Users/daparicio/Library/Caches/hvm/v0.121.2/hugo server --port 1313 "${HUGO_DEBUG}" --baseURL "${BASE_URL}" #--i18n-warnings --disableFastRender 
+#hugo server --port 1313 "${HUGO_DEBUG}" --baseURL "${BASE_URL}" #--i18n-warnings --disableFastRender 
